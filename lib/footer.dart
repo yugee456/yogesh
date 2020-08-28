7ae904c0d4e9f0dee7309872e9d2e4dfe0d32545
Yogesh <yogeshjaisnkar@gmail.com>
2020-08-28 22:20:28 +0530

    Initial commit

---
+++ lib/footer.dart
@@ -0,0 +1,72 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'colors.dart';
+import 'header.dart';
+
+class FooterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return VStack(
+      [
+        VxDevice(
+          mobile: VStack(
+            [
+              "Got a project?\nLet's talk.".text.center.white.xl2.make(),
+              10.heightBox,
+              "yogeshjai.mailme@gmail.com"
+                  .text
+                  .color(Coolers.accentColor)
+                  .semiBold
+                  .make()
+                  .box
+                  .border(color: Coolers.accentColor)
+                  .p16
+                  .rounded
+                  .make()
+            ],
+            crossAlignment: CrossAxisAlignment.center,
+          ),
+          web: HStack(
+            [
+              "Got a project?\nLet's talk.".text.center.white.xl2.make(),
+              10.widthBox,
+              "yogeshjai.mailme@gmail.com"
+                  .text
+                  .color(Coolers.accentColor)
+                  .semiBold
+                  .make()
+                  .box
+                  .border(color: Coolers.accentColor)
+                  .p16
+                  .rounded
+                  .make(),
+            ],
+            alignment: MainAxisAlignment.spaceAround,
+          ).w(context.safePercentWidth * 70).scale150().p16(),
+        ),
+        50.heightBox,
+        CustomAppBar(),
+        10.heightBox,
+        "Thanks for scrolling, ".richText.semiBold.white.withTextSpanChildren(
+            ["that's all folks.".textSpan.gray500.make()]).make(),
+        10.heightBox,
+        SocialAccounts(),
+        30.heightBox,
+        [
+          "Made in India with".text.red500.make(),
+          10.widthBox,
+          Icon(
+            AntDesign.heart,
+            color: Vx.red500,
+            size: 14,
+          )
+        ].hStack(crossAlignment: CrossAxisAlignment.center)
+      ],
+      crossAlignment: CrossAxisAlignment.center,
+    ).wFull(context).p16();
+  }
+}
---
+++ lib/header.dart
@@ -0,0 +1,168 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'colors.dart';
+
+class HeaderScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final nameWidget = "Yogesh\nJaisankar"
+        .text
+        .white
+        .xl5
+        .lineHeight(1)
+        .size(context.isMobile ? 15 : 20)
+        .bold
+        .make()
+        .shimmer();
+    return SafeArea(
+      child: VxBox(
+              child: VStack([
+        ZStack(
+          [
+            PictureWidget(),
+            Row(
+              children: [
+                VStack([
+                  if (context.isMobile) 50.heightBox else 10.heightBox,
+                  CustomAppBar().shimmer(primaryColor: Coolers.accentColor),
+                  30.heightBox,
+                  nameWidget,
+                  20.heightBox,
+                  VxBox()
+                      .color(Coolers.accentColor)
+                      .size(60, 10)
+                      .make()
+                      .px4()
+                      .shimmer(primaryColor: Coolers.accentColor),
+                  30.heightBox,
+                  SocialAccounts(),
+                ]).pSymmetric(
+                  h: context.percentWidth * 10,
+                  v: 32,
+                ),
+                Expanded(
+                  child: VxResponsive(
+                    medium: IntroductionWidget()
+                        .pOnly(left: 120)
+                        .h(context.percentHeight * 60),
+                    large: IntroductionWidget()
+                        .pOnly(left: 120)
+                        .h(context.percentHeight * 60),
+                    fallback: const Offstage(),
+                  ),
+                )
+              ],
+            ).w(context.screenWidth)
+          ],
+        )
+      ]))
+          .size(context.screenWidth, context.percentHeight * 60)
+          .color(Coolers.secondaryColor)
+          .make(),
+    );
+  }
+}
+
+class IntroductionWidget extends StatelessWidget {
+  const IntroductionWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VStack(
+      [
+        [
+          " - Introduction".text.gray500.widest.sm.make(),
+          10.heightBox,
+          "@Devexpert for Flutter, React, Firebase, Dart & Web.\nChild prodigy in programming and machine language.\nFounder of MTechViral."
+              .text
+              .white
+              .xl3
+              .maxLines(5)
+              .make()
+              .w(context.isMobile
+                  ? context.screenWidth
+                  : context.percentWidth * 40),
+          20.heightBox,
+        ].vStack(),
+      ],
+      // crossAlignment: CrossAxisAlignment.center,
+      alignment: MainAxisAlignment.spaceEvenly,
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      AntDesign.codesquare,
+      size: 50,
+      color: Coolers.accentColor,
+    );
+  }
+}
+
+class PictureWidget extends StatelessWidget {
+  const PictureWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      alignment: Alignment.center,
+      origin: Offset(context.percentWidth * 14, 0),
+      transform: Matrix4.rotationY(pi),
+      child: Image.asset(
+        "assets/perfect.png",
+        fit: BoxFit.cover,
+        height: context.percentHeight * 60,
+      ),
+    );
+  }
+}
+
+class SocialAccounts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return [
+      Icon(
+        AntDesign.twitter,
+        color: Colors.white,
+      ).mdClick(() {
+        launch("https://twitter.com/YogeshJaisankar");
+      }).make(),
+      20.widthBox,
+      Icon(
+        AntDesign.instagram,
+        color: Colors.white,
+      ).mdClick(() {
+        launch("https://www.instagram.com/_mr.cleverprogrammer_/");
+      }).make(),
+      20.widthBox,
+      Icon(
+        AntDesign.facebook_square,
+        color: Colors.white,
+      ).mdClick(() {
+        launch("https://www.facebook.com/yogesh.jaisankar.9");
+      }).make(),
+      20.widthBox,
+      Icon(
+        AntDesign.github,
+        color: Colors.white,
+      ).mdClick(() {
+        launch("https://github.com/yugee456");
+      }).make()
+    ].hStack();
+  }
+}
